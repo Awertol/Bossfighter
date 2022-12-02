c5d1d92a1a669319a324196149f85a9620cd687c
Michal Helgert <michal.helgert@seznam.cz>
2022-12-02 07:48:13 +0100

    Přidejte soubory projektu.

---
+++ Bossfighter/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfighter
+{
+    internal class Enemy
+    {
+        public string Obrazek;
+        public int Utok;
+        public int HP;
+    }
+}
